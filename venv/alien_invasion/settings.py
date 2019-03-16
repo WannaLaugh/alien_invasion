62b3c4385e4d40b16e7f25bce7d202c7d8b34adc
golden <691207257@qq.com>
2019-03-16 17:41:46 +0800

    modify and check

--- venv/alien_invasion/settings.py
+++ venv/alien_invasion/settings.py
@@ -7,13 +7,13 @@ class Settings:
         """初始化游戏的设置"""
         # 屏幕设置
         self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
 
         # 子弹设置
         self.bullet_speed_factor = 2
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 230, 0, 0
         self.bullets_allowed = 5
